8421bb5afc222b1610822ab131fcb8e8b361ed74
goncy <gonzalo.pozzo4@gmail.com>
2020-02-01 10:11:17 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Alert from "./components/Alert.svelte";
+  import Button from "./components/Button.svelte";
+  import Card from "./components/Card.svelte";
+</script>
+
+<main>
+  <Button>Blue</Button>
+  <Button color="red">Red</Button>
+
+  <div class="my-4">
+    <Card />
+  </div>
+
+  <Alert />
+</main>
---
+++ src/components/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let color = "blue";
+
+  const variants = {
+    blue: "border-blue-500 border-blue-500 hover:border-blue-600 hover:bg-blue-500",
+    red: "border-red-500 border-red-500 hover:border-red-600 hover:bg-red-500",
+  };
+</script>
+
+<button
+  class={`w-32 bg-white tracking-wide text-gray-800 font-bold rounded border-0 border-b-2 hover:text-white shadow-md py-2
+  px-6 inline-flex items-center ${variants[color]}`}>
+  <span class="mx-auto">
+    <slot />
+  </span>
+</button>
